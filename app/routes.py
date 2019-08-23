1833b2d8d3d7b94ec439e1672d9c075ecfb6ba60
Alexander Golovnya <algol.mail@gmail.com>
2019-08-23 12:00:53 +0700

    Add submit and validator

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -30,7 +30,14 @@ def index():
     return render_template('index.html', title='Home', user=user, users=users)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f'Login requested for user {form.username.data}, '
+            f'remember_me={form.remember_me.data}'
+        )
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
+
